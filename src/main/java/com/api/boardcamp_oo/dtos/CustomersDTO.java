38c6a1f8696f8d087ea3a1169a6aa4077fd1802c
Annylory LR <annyloryrosa@gmail.com>
2025-07-16 16:04:22 -0300

    finished dtos

--- src/main/java/com/api/boardcamp_oo/dtos/CustomersDTO.java
+++ src/main/java/com/api/boardcamp_oo/dtos/CustomersDTO.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class CustomersDTO {
     
     @NotBlank
--- src/main/java/com/api/boardcamp_oo/dtos/RentalsDTO.java
+++ src/main/java/com/api/boardcamp_oo/dtos/RentalsDTO.java
@@ -1,5 +1,27 @@
 package com.api.boardcamp_oo.dtos;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
 public class RentalsDTO {
     
+    @NotNull
+    private LocalDate renDate;
+
+    @NotNull
+    @Min(1)
+    private Integer daysRented;
+
+    private LocalDate returnDate;
+
+    @NotNull
+    private Long originalPrice;
+
+    @PositiveOrZero
+    private Long delayFee;
 }
