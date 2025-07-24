f95c3c5b70b9ca26cbdd32395b97f782830c66bf
Annylory LR <annyloryrosa@gmail.com>
2025-07-24 15:22:40 -0300

    organizing

--- src/test/java/com/api/boardcamp_oo/unitTests/RentalsUnitTests.java
+++ src/test/java/com/api/boardcamp_oo/unitTests/RentalsUnitTests.java
@@ -1,4 +1,4 @@
-package com.api.boardcamp_oo;
+package com.api.boardcamp_oo.unitTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
